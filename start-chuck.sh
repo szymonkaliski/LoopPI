51e154edec776d3ea04aba0e7e6e4e036f40c62b
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-07-26 12:52:40 -0600

    updated chuck / jack starters

--- start-chuck.sh
+++ start-chuck.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-(sleep 5s && (
-	jack_connect "alsa_in:capture_1" "ChucK:inport 0" &
-	jack_connect "ChucK:outport 0" "alsa_out:playback_1" &
-	jack_connect "ChucK:outport 1" "alsa_out:playback_2" &
-)) &
+# (sleep 5s && (
+# 	jack_connect "alsa_in:capture_1" "ChucK:inport 0" &
+# 	jack_connect "ChucK:outport 0" "alsa_out:playback_1" &
+# 	jack_connect "ChucK:outport 1" "alsa_out:playback_2" &
+# )) &
 
 echo "ChucK => started"
 chuck --loop --bufsize4096 --in1
--- start-jack.sh
+++ start-jack.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-QUALITY=4
+# QUALITY=1
 
-(sleep 5s && (
-	alsa_out -j "alsa_out" -d hw:1,0 -c 2 -r 44100 -q $QUALITY &
-	alsa_in -j "alsa_in" -d hw:1,0 -c 1 -q $QUALITY &
-)) &
+# (sleep 5s && (
+# 	alsa_out -j "alsa_out" -d hw:1,0 -c 2 -r 44100 -q $QUALITY &
+# 	alsa_in -j "alsa_in" -d hw:1,0 -c 1 -q $QUALITY &
+# )) &
 
-jackd -d dummy -r
+# jackd -d dummy -r -p 8
+
+
+jackd -P70 -p16 -dalsa -dhw:1,0 -p128 -r44100 -i1 -o2 -n3 -s
 
