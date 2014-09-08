08987fc8b897b82155bda410ad177ce8d9b56e27
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-09-08 15:14:15 -0600

    shorter loops and longer startup wait time

--- start.js
+++ start.js
@@ -12,17 +12,14 @@ var logStd = function(spawned) {
 var jackd = spawn("jackd", [ "-P60", "-p8", "-dalsa", "-dhw:1,0", "-i1", "-o2", "-n3", "-r44100", "-s", "-S", "-znone" ]);
 logStd(jackd);
 
-// wait 1 second and start chuck
+// start chuck and controller after timeout
 setTimeout(function() {
-	var chuckLooper = spawn("chuck", [ "--in1", __dirname + "/Looper/looper.ck"]);
+	var chuckLooper = spawn("chuck", [ "--in1", "--adaptive:512", __dirname + "/Looper/looper.ck"]);
 	logStd(chuckLooper)
-}, 1000);
 
-// wait another second and start controller app
-setTimeout(function() {
 	var controller = spawn("node", [ __dirname + "/Controller/app.js" ]);
 	logStd(controller);
-}, 2000);
+}, 4000);
 
 process.on("SIGINT", function() {
 	// force kill jack
